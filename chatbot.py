592d35839e4627f06e398efb5bf2388dd61d7a08
eroj333 <shresthaenosh35@yahoo.com>
2017-02-06 21:46:44 +0545

    Add query for events

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 from wit import Wit
 from app import db
 from models import Company, Events
+import json
 access_token = 'ERIZHU2Q3IYPCEMCXKGEL3DZIR3RQN4A'
 
 def processUserText(text):
@@ -12,9 +13,9 @@ def prepareReply(hasResult, all_result, resultType):
     reply = {}
     reply['hasResults'] = hasResult
     reply['results'] = all_result
-    reply['resultType'] = 'place'
-    reply['botReply'] = 'Query Complete. Showing ' + resultType + 'nearby'
-    return reply
+    reply['resultType'] = resultType
+    reply['botReply'] = 'Query Complete. Showing results'
+    return json.dumps(reply)
 
 def getRestaurants():
     restaurants = db.session.query(Company).filter_by(org_type='Restaurant').all()
@@ -31,16 +32,39 @@ def getRestaurants():
         dict_data['image'] = dict_data.pop('logo')
         dict_data['desc'] = dict_data.pop('description')
         all_results.append(dict_data)
-    reply = prepareReply(hasResults, all_results)
-    print reply
-    return reply
+    return hasResults, all_results
 
+def getEvents():
+    events = db.session.query(Events).all()
+    hasResults = len(events)>0
+    all_events = []
+    for event in events:
+        location, lat, lng = event.location.split(',')
+        eventDict = event.__dict__
+        eventDict.pop('location')
+        eventDict['location'] = location
+        eventDict['lat'] = lat
+        eventDict['long'] = lng
+        eventDict.pop('_sa_instance_state')
+        eventDict['image'] = eventDict.pop('banner')
+        all_events.append(eventDict)
+    return hasResults, all_events
 
-def sendReply():
-    keyword = processUserText('Could you find me places to eat');
+def sendReply(userText):
+    keyword = processUserText(userText)
     if keyword == 'restaurants and bar':
-        return getRestaurants()
-    return getEvents()
+        has_result, result = getRestaurants()
+    elif keyword == 'event' or keyword=='events':
+        has_result, result = getEvents()
+    #elif keyword == 'none':
+    else:
+        has_result1, result1 = getEvents() 
+        has_result2, result2 = getRestaurants()
+        has_result = has_result1 or has_result2
+        result = result1 + result2
+    
+    reply = prepareReply(has_result, result, keyword)
+    return reply
         
 if __name__ == '__main__':
-    sendReply()
+    sendReply('find restaurants nearby')
